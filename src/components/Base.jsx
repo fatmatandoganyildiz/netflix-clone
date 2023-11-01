5fae51378f1d05b0fff4e1871366cdf745f5b0e0
Fatma Tandogan Yildiz <fatmatandogan609@gmail.com>
2023-11-01 18:15:56 +0000

    Row-Movie Pages

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -12,7 +12,15 @@ const Base = () => {
     });
   }, []);
 
-  console.log(movie);
+  //   console.log(movie);
+
+  const truncateString = (str, num) => {
+    if (str?.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+  };
 
   return (
     <div className="w-full h-[550px] text-white">
@@ -23,14 +31,21 @@ const Base = () => {
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt={movie?.title}
         />
-        <div className="">
+        <div className="absolute w-full top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
           <div>
-            <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
+            <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5 my-4">
               Play
             </button>
-            <button className="border text-white border-gray-300 py-2 px-5 ml-4">
+            <button className="border text-white border-gray-300 py-2 px-5 ml-4 my-4">
               Watch Later
             </button>
+            <p className="text-gray-400 text-sm">
+              Released: {movie?.release_date}
+            </p>
+            <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">
+              {truncateString(movie?.overview, 150)}
+            </p>
           </div>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import Base from '../components/Base'
+import Row from '../components/Row'
+import requests from '../Request'
 const Home = () => {
   return (
     <div>
       <Base/>
+      <Row rowID='1' title="Popular" fetchURL={requests.requestPopular}/>
+      <Row rowID='2' title="Up Coming" fetchURL={requests.requestUpcoming}/>
+      <Row rowID='3' title="Top Rated" fetchURL={requests.requestTopRated}/>
+      <Row rowID='4' title="Trending" fetchURL={requests.requestTrending}/>
+      <Row rowID='5' title="Horror" fetchURL={requests.requestHorror}/>
     </div>
   )
 }
