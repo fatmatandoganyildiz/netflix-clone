9b4c21003474a7a5d1c2f1223d37166f41f7ddbe
Fatma Tandogan Yildiz <fatmatandogan609@gmail.com>
2023-11-09 10:21:25 +0000

    End of the Project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthContextProvider } from "./context/AuthContext";
+import Account from "./pages/Account";
 import Home from "./pages/Home";
-import { Routes, Route } from 'react-router-dom';
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <h1 className="text-white">
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-      </Routes>
-    </h1>
+    <AuthContextProvider>
+      <h1 className="text-white">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route
+            path="/account"
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </h1>
+    </AuthContextProvider>
   );
 }
 
